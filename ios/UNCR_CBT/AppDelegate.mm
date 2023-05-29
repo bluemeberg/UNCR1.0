69ba9ce2c397cfc5639b45f8961955a3c0fffccf
김학준 <khjun@gimhagjun-ui-noteubug.local>
2023-05-29 20:47:10 +0900

    230529_all

--- ios/UNCR_CBT/AppDelegate.mm
+++ ios/UNCR_CBT/AppDelegate.mm
@@ -1,3 +1,4 @@
+#import <React/RCTLinkingManager.h> // 파일 상단에 추가
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -32,6 +33,13 @@
 - (BOOL)concurrentRootEnabled
 {
   return true;
+}
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
